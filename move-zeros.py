5c92d23b0d3adfaadf856dce2712b0fa1a6b9f7d
Ali-Al-Ahmad <ali.alahmad.cs@gmail.com>
2024-11-13 22:12:07 +0200

    clean code

--- move-zeros.py
+++ move-zeros.py
@@ -1,19 +1,23 @@
-numbers_array = [0,0,5,8,0,1,0,4,12,0]
-#               [5,8,1,4,12,0,0,0,0,0]
-length_of_array = len(numbers_array)
+"this function moves all 0 to the most right side of the list"
 
-def move_zeros(numbers):
-    
-    for i in range(length_of_array):
-        for j in range(length_of_array-1):
-            
+numbers_array = [0, 0, 5, 8, 0, 1, 0, 4, 12, 0]
+#               [5,8,1,4,12,0,0,0,0,0]
+LENGTH_OF_ARRAY = len(numbers_array)
+
+
+def _move_zeros(numbers):
+
+    for i in range(LENGTH_OF_ARRAY):
+        for j in range(LENGTH_OF_ARRAY-1):
+
             # check if number is zero move it to right by swapping it
             if numbers[j] == 0:
                 temp = numbers[j+1]
                 numbers[j+1] = numbers[j]
                 numbers[j] = temp
-                
-    return numbers 
 
-new_array = move_zeros(numbers_array)   
-print(new_array)      
+    return numbers
+
+
+new_array = _move_zeros(numbers_array)
+print(new_array)
